aad47a0a171eeadc19e644c68cee3d7e9e1b20f3
mramirid <amir.muh.hakim@gmail.com>
2023-02-09 06:31:19 +0700

    feat: add a simple web server

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import http from "http";
+
+const PORT = 3000;
+const HOSTNAME = "localhost";
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ message: "Hello World" }));
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
